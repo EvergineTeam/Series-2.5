7b8d16740b06ae5d0d60baf363a246ada47f8655
Antonio Ávila Membrives <antonio.avila@gmail.com>
2015-10-22 20:08:32 +0100

    - Added 6th chapter for the 3d gamming programming series

---
+++ SpaceShipDemo/06-Behaviors-2/SpaceShipDemo/SharedSource/Main/FollowBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using WaveEngine.Common.Attributes;
+using WaveEngine.Common.Math;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+
+namespace SpaceShipDemo
+{
+    [DataContract]
+    public class FollowBehavior : Behavior
+    {
+        [RequiredComponent]
+        public Transform3D Transform;
+
+        [DataMember]
+        [RenderPropertyAsEntity(new string[] { "WaveEngine.Framework.Graphics.Tranform3D" })]
+        public string EntityPath { get; set; }
+
+        private Transform3D targetTransform;
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            if (string.IsNullOrEmpty(this.EntityPath))
+            {
+                return;
+            }
+
+            var entity = this.EntityManager.Find(this.EntityPath);
+            this.targetTransform = entity.FindComponent<Transform3D>();
+        }
+
+        protected override void Update(TimeSpan gameTime)
+        {
+            if (this.targetTransform == null)
+            {
+                return;
+            }
+
+            var lerp = Math.Min(1, 10 * (float)gameTime.TotalSeconds);
+
+            this.Transform.Position = Vector3.Lerp(this.Transform.Position, this.targetTransform.Position, lerp);
+            this.Transform.Rotation = Vector3.Lerp(this.Transform.Rotation, this.targetTransform.Rotation, lerp);
+        }
+    }
+}
---
+++ SpaceShipDemo/06-Behaviors-2/SpaceShipDemo/SharedSource/Main/ShipBehavior.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using WaveEngine.Common.Math;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Services;
+
+namespace SpaceShipDemo
+{
+    [DataContract]
+    public class ShipBehavior : Behavior
+    {
+        [RequiredComponent]
+        public Transform3D Tranform;
+
+        [DataMember]
+        public float Speed { get; set; }
+
+        private float currentSpeed;
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            this.currentSpeed = this.Speed;
+        }
+
+        protected override void Update(TimeSpan gameTime)
+        {
+            var rotation = Vector3.Zero;
+
+            var input = WaveServices.Input.KeyboardState;
+
+            if (input.W == WaveEngine.Common.Input.ButtonState.Pressed)
+            {
+                rotation.Y -= (float)gameTime.TotalSeconds;
+            }
+
+            if (input.S == WaveEngine.Common.Input.ButtonState.Pressed)
+            {
+                rotation.Y += (float)gameTime.TotalSeconds;
+            }
+
+            if (input.A == WaveEngine.Common.Input.ButtonState.Pressed)
+            {
+                rotation.X -= (float)gameTime.TotalSeconds;
+            }
+
+            if (input.D == WaveEngine.Common.Input.ButtonState.Pressed)
+            {
+                rotation.X += (float)gameTime.TotalSeconds;
+            }
+
+            this.Tranform.LocalOrientation *= Quaternion.CreateFromYawPitchRoll(rotation.X, rotation.Y, rotation.Z);
+
+            var localPosition = this.Tranform.LocalPosition;
+
+            localPosition.Z -= (float)(this.currentSpeed * gameTime.TotalSeconds);
+
+            this.Tranform.LocalPosition = localPosition;
+        }
+    }
+}
